616c0828ca28909520b00eaf983d7dcb0bea7aff
joseluismillan <joseluismillan@gmail.com>
2020-12-12 01:26:29 -0300

    added imgs3

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,7 +7,13 @@ const awsmobile = {
     "aws_cognito_region": "us-east-1",
     "aws_user_pools_id": "us-east-1_9LhjsLwiP",
     "aws_user_pools_web_client_id": "7fgvs50m0urlm7egbnkhric6id",
-    "oauth": {}
+    "oauth": {},
+    "aws_appsync_graphqlEndpoint": "https://pannitsv7jbz5cz2qoa6gy4zb4.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-6scsjuohpzer7dvjwnhvzeoije",
+    "aws_user_files_s3_bucket": "bucketrioblue24856-dev",
+    "aws_user_files_s3_bucket_region": "us-east-1"
 };
 
 
